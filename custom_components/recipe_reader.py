85b475d56d08d87ff6664e946b02ec6894f878b7
chris3145 <chris3145@gmail.com>
2017-03-20 09:17:44 -0500

    Declare rcpTitle as global?

--- custom_components/recipe_reader.py
+++ custom_components/recipe_reader.py
@@ -364,7 +364,7 @@ def setup(hass, config):
     hass.services.register(DOMAIN, 'findAmount', findAmount)
     
     # populate default parameters before the recipe file is read
-    rcpTitle = 'no title yet'
+    global rcpTitle = 'no title yet'
 
     # # print('Previous title:', rcpTitle)
     
